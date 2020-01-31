609ede50fad2da7f8866f007e88c78be2a7fc14a
ibrahimshamma99 <i.abushammah@gmail.com>
2020-01-31 18:10:31 +0200

    connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+  GraphQLModule.forRoot({
+    autoSchemaFile: 'schema.gql',
+  }),
+  ItemsModule,
+  MongooseModule.forRoot('mongodb://localhost/nestgraphql')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,37 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { ItemsService } from './items.service';
+import { ItemType } from './dto/create-item.dto';
+import { ItemInput } from './input-items.input';
 
-@Resolver('Items')
-export class ItemsResolver {}
+@Resolver()
+export class ItemsResolver {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Query(() => [ItemType])
+  async items(): Promise<ItemType[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Mutation(() => ItemType)
+  async createItem(@Args('input') input: ItemInput): Promise<ItemInput> {
+    return this.itemsService.create(input);
+  }
+
+  @Mutation(() => ItemType)
+  async updateItem(
+    @Args('id') id: string,
+    @Args('input') input: ItemInput,
+  ): Promise<ItemInput> {
+    return this.itemsService.update(id, input);
+  }
+
+  @Mutation(() => ItemType)
+  async deleteItem(@Args('id') id: string): Promise<ItemInput> {
+    return this.itemsService.delete(id);
+  }
+
+  @Query(() => String)
+  async hello() {
+    return 'hello';
+  }
+}
